bf9d79adf68b7d09da4b8fd51a94d96aa56fc15c
Nestorsr1115 <nestorsr1115@gmail.com>
2025-05-20 12:17:38 -0600

    verificacion

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ config = rx.Config(
     app_name="mouredev",
 
 head_tags=[
-        "<meta name='google-site-verification: google97886930ceb23f0c.html' />"
+        "<meta name='google-site-verification' content='97886930ceb23f0c' />"
     ],
 )
 
